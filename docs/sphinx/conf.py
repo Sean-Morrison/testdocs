bfc9690c312082d5e87fa0dca49fee7a15fce80d
Sean Morrison <41479278+Sean-Morrison@users.noreply.github.com>
2024-08-09 19:20:58 -0500

    Update conf.py

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -106,7 +106,7 @@ language = "en"
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
 if 'html' in sys.argv:
-    tag.add('html')
+    tags.add('html')
 
 exclude_patterns = ['sos.rst'] if tags.has('html') else []
 exclude_patterns.extend(['_build', 'Thumbs.db', '.DS_Store'])
