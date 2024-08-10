69e3d3243627e3c3e7b1ed6e19c97a2351378a74
Sean Morrison <41479278+Sean-Morrison@users.noreply.github.com>
2024-08-10 02:27:13 -0500

    Update conf.py

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -199,7 +199,7 @@ def setup_sidebarTOC(app, pagename, templatename, context, doctree):
         else:
             toctree_html = ""
         return get_navigation_tree(toctree_html)
-    context['sidebarTOC'] = sidebarTOC
+    context['sidebarTOC'] = sidebarTOC(context)
 
 
 def setup(app):
